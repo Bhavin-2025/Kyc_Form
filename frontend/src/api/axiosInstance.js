dbcce9c5c4ff19537149b2c18363baa33e3f887e
Bhavin Giniya <bhavin@sarvadhi.com>
2025-09-20 09:43:49 +0530

    .env and axios Instance

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,19 @@
+// // src/api/axiosInstance.js
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://p5tgmv3m-5000.inc1.devtunnels.ms/api", // ✅ your backend API
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default axiosInstance;
 // src/api/axiosInstance.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://p5tgmv3m-5000.inc1.devtunnels.ms/api", // ✅ your backend API
+  baseURL: import.meta.env.VITE_API_URL, // ✅ use environment variable
   headers: {
     "Content-Type": "application/json",
   },
